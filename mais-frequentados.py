b1c0516dd416529497f65dbae6a7acf7c8592e8c
Luana Paes <114548504+luanapaes@users.noreply.github.com>
2023-09-30 09:17:56 -0300

    Update mais-frequentados.py

--- mais-frequentados.py
+++ mais-frequentados.py
@@ -1,4 +1,4 @@
-#GRÁFICO DE BARRAS
+# GRAFICO DE BARRAS
 import pandas as pd
 
 # carregando o dataframe
@@ -7,7 +7,7 @@ df = pd.read_csv('data.csv')
 # Converta o DataFrame em um dicionário - cada coluna virou um "array"
 data_dict = df.to_dict(orient='list')
 
-# Acessa cada coluna do dicionário e colocada cada uma em uma variável
+# Acessa cada coluna do dicionário e atribua-a a uma variável
 months_as_member = data_dict['months_as_member']
 weight = data_dict['weight']
 days_before = data_dict['days_before']
@@ -16,13 +16,11 @@ time = data_dict['time']
 category = data_dict['category']
 attended = data_dict['attended']
 
-import plotly.express as px
-import pandas as pd
 
 import plotly.express as px
 import pandas as pd
 
-# Crie um DataFrame com suas variáveis
+# Cria um DataFrame com suas variáveis
 df = pd.DataFrame({
     'day_of_week': day_of_week,
     'time': time
@@ -31,11 +29,11 @@ df = pd.DataFrame({
 # Cria uma coluna com a contagem de ocorrências para cada combinação de dia da semana e hora
 df_grouped = df.groupby(['day_of_week', 'time']).size().reset_index(name='count')
 
-# Calcule as contagens para AM e PM
+# Calcula as contagens para AM e PM
 am_count = df_grouped[df_grouped['time'] == 'AM']['count'].sum()
 pm_count = df_grouped[df_grouped['time'] == 'PM']['count'].sum()
 
-# Determina qual período tem maior contagem
+# Determina qual período tem maior contagem, se é de manhã ou tarde e noite
 if am_count > pm_count:
     maior_periodo = "entre 00h e 12h o movimento é maior"
 else:
@@ -46,7 +44,7 @@ fig = px.bar(df_grouped, x='day_of_week', y='count', color='time',
              labels={'day_of_week': 'Dia da Semana', 'count': 'Hora'},
              title='Gráfico de Barras: Contagem por Dia da Semana e Hora')
 
-# Adiciona um parágrafo de texto com base no período com a maior contagem
+# Adicione um parágrafo de texto com base no período com maior contagem
 fig.add_annotation(
     text=maior_periodo,
     x=0.5,  # Posição x no gráfico (0 a 1)
@@ -57,7 +55,7 @@ fig.add_annotation(
     bordercolor="gray",
     borderwidth=1,
     borderpad=4,
-    opacity=0.7
+    opacity=0.9
 )
 
 fig.show()
